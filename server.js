bf3aecaa1f8869bd60f794cd5749d3a336a3c332
Sam Feder <sam.feder1@gmail.com>
2020-04-17 12:46:04 -0400

    add debugability

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ var app = express()
 app.use(cors())
 
 const port = 3000
+const topics = [{name: 'NFL Draft', id: 1}, {name: 'MLB Players', id: 2}, {name: 'NBA Playoffs', id: 3}]
 
-app.get('/topics', (req, res) => res.send([
-  {name: 'NFL Draft', id: 1}, {name: 'MLB Players', id: 2}, {name: 'NBA Playoffs', id: 3}
-]))
+app.get('/topics', (req, res) => res.send(topics))
+
+app.get('/topics/:id', (req, res) => res.send(
+  topics.find(topic => +req.params.id === topic.id)
+))
 
 app.listen(port, () => console.log(`Server live at http://localhost:${port}`))
