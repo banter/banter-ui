8b989e4dd0c13ed87cf7ee83561c734c135b2846
Sam Feder <sam.feder1@gmail.com>
2020-06-02 21:41:55 -0400

    use aws API

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const API = {
-  BASE_URL: 'http://localhost:8080/api/v1/',
+  BASE_URL: 'http://bante-loadb-4u6ir1e2setb-1808666837.us-east-1.elb.amazonaws.com/api/v1/',
   TOPICS: "topics/",
   GENRES: "genres/"
 };
