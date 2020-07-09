1d1792f57667283230aa58d80b600ece91dbb45f
Sam Feder <sam.feder1@gmail.com>
2020-07-08 22:11:07 -0400

    add vue analytics

--- src/main.js
+++ src/main.js
@@ -4,14 +4,22 @@ import Vuex from 'vuex'
 import { store } from "./store/index.js";
 import { router } from "./router/index.js";
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueAnalytics from 'vue-analytics';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import moment from 'moment'
 
+
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.use(Vuex)
+
+Vue.use(VueAnalytics, {
+  id: 'UA-172119455-1',
+  router
+})
+
 Vue.config.productionTip = false
 
 moment.locale("en-us");
