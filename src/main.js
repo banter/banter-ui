7d5d74a23f9e31bc44cfef9efb18fc915d51de8c
Sam Feder <sam.feder1@gmail.com>
2020-07-11 11:45:16 -0400

    add hotjar

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import moment from 'moment';
 import Carousel3d from 'vue-carousel-3d';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import {
+  BootstrapVue,
+  IconsPlugin,
+} from 'bootstrap-vue';
+import Hotjar from 'vue-hotjar';
 import store from './store/index';
 import router from './router/index';
 import App from './App.vue';
@@ -12,6 +16,10 @@ import Landing from './layouts/Landing.vue';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(Hotjar, {
+  id: '1897223',
+});
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
@@ -25,7 +33,9 @@ Vue.use(Vuex);
 Vue.config.productionTip = false;
 
 moment.locale('en-us');
-Object.defineProperty(Vue.prototype, '$moment', { value: moment });
+Object.defineProperty(Vue.prototype, '$moment', {
+  value: moment,
+});
 
 Vue.component('default-layout', Default);
 Vue.component('landing-layout', Landing);
