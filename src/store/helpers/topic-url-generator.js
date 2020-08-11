85be30598af6ed25891b7441a77d4c5b8f431ca1
Sam Feder <sam.feder1@gmail.com>
2020-08-11 12:15:13 -0400

    make some code a little sexier

--- src/store/helpers/topic-url-generator.js
+++ src/store/helpers/topic-url-generator.js
@@ -1,9 +1,5 @@
 export default function topicUrlGenerator(topic, isSearchBar) {
-  let name = '';
-  if (!isSearchBar) {
-    name = topic.value.replace(/\s+/g, '-').toLowerCase();
-  } else {
-    name = topic.label.replace(/\s+/g, '-').toLowerCase();
-  }
+  const attr = isSearchBar ? 'label' : 'value';
+  const name = topic[attr].replace(/\s+/g, '-').toLowerCase();
   return `/topics/${name}/${topic.id}`;
 }
