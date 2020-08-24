d7a6be0c73ba4207d40797fe4a9db85eb0a60557
Austin <amarchese3@gmail.com>
2020-08-24 19:02:41 -0400

    Adding image to content

--- src/App.vue
+++ src/App.vue
@@ -13,27 +13,28 @@ export default {
       return `${this.$route.meta.layout || 'default'}-layout`;
     },
   },
-  //   data() {
-  //   return {
-  //     logo: ROOT_PATH + require('./assets/logo.png')
-  //   }
-  // },
+  data() {
+    return {
+      // eslint-disable-next-line global-require
+      logo: window.location.origin + require('./assets/Banter_logo_dark.png'),
+    };
+  },
   metaInfo() {
     return {
 
-      title: 'Epiloge - Build your network in your field of interest',
+      title: 'Banter',
       meta: [
         // Twitter Card
         { name: 'twitter:card', content: 'summary' },
-        { name: 'twitter:title', content: 'Vue Social Cards Example' },
+        { name: 'twitter:title', content: 'Banter' },
         { name: 'twitter:description', content: 'Vue sample site showing off Twitter and Facebook Cards.' },
         // image must be an absolute path
-        // { name: 'twitter:image', content: this.logo },
+        { name: 'twitter:image', content: this.logo },
         // Facebook OpenGraph
         { property: 'og:title', content: 'Vue Social Cards Example' },
         { property: 'og:site_name', content: 'Vue Example' },
         { property: 'og:type', content: 'website' },
-        // { property: 'og:image', content: this.logo },
+        { property: 'og:image', content: this.logo },
         { property: 'og:description', content: 'Vue sample site showing off Twitter and Facebook Cards.' },
       ],
     };
